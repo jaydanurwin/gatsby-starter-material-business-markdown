42b86da49a202011fb5785b8d71f26217be671aa
Urwin <jurwin1@SDGL15069e1e1.local>
2019-01-23 08:27:10 -0700

    changed class to About

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,30 @@
-import React from "react"
-import { graphql, Link } from "gatsby"
+import React from "react";
+import { graphql, Link } from "gatsby";
 
-const Index = ({ data: { prismicAboutpage } }) => (
+const About = ({ data: { prismicAboutpage } }) => (
     <React.Fragment>
         <h1>{prismicAboutpage.data.title.text}</h1>
         <div
-            dangerouslySetInnerHTML={{ __html: prismicAboutpage.data.content.html }}
+            dangerouslySetInnerHTML={{
+                __html: prismicAboutpage.data.content.html
+            }}
         />
     </React.Fragment>
-)
+);
 
-export default Index
+export default About;
 
 export const pageQuery = graphql`
-  query {
-    prismicAboutpage {
-      data {
-        title {
-          text
+    query {
+        prismicAboutpage {
+            data {
+                title {
+                    text
+                }
+                content {
+                    html
+                }
+            }
         }
-        content {
-          html
-        }
-      }
     }
-  }
-`
+`;
