5c212c720feebc8d8c72e290417ce9c3514f92b7
Urwin <jurwin1@SDGL15069e1e1.local>
2019-01-23 14:47:48 -0700

    added typography components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,23 +2,38 @@ import React from "react";
 import footerProfile from "../../static/images/footer-profile.png";
 import instagramOutlineLightTheme from "../../static/images/instagram-logo-outline--lightTheme.svg";
 import idahoOutlineLightTheme from "../../static/images/idaho-outline--lightTheme.svg";
+import {
+    Body1,
+    Body2,
+    Button,
+    Caption,
+    Headline1,
+    Headline2,
+    Headline3,
+    Headline4,
+    Headline5,
+    Headline6,
+    Overline,
+    Subtitle1,
+    Subtitle2,
+} from '@material/react-typography';
 
 // Styles
 import "../styles/app.scss";
 
 export default () => (
     <section className="anoun-hero-section">
-        <h3>
+        <Headline3>
             Hello, we're <span className="anoun-title">ANOUN</span>.
-        </h3>
-        <h4>We believe this.</h4>
-        <p>
+        </Headline3>
+        <Headline4>We believe this.</Headline4>
+        <Body1>
             We believe design is in everything. Whether it's a person, place,
             thing or idea. We believe design should be done right from the
             largest to the smallest detail. No if's, and's, or <span role="img" aria-label="butts">🍑</span>'s  about it. We don't cut corners and we don't ship anything half
             baked or incomplete.
-        </p>
-        <h4>
+        </Body1>
+        <Headline4>
             Made in{" "}
             <img
                 src={idahoOutlineLightTheme}
@@ -27,8 +42,8 @@ export default () => (
                 id="idahoImage"
                 alt="idaho-state-vector-outline-svg"
             />
-        </h4>
-        <p>
+        </Headline4>
+        <Body1>
             Although I currently call Nampa, Idaho home I can meet anywhere in
             Boise or Meridian (as long as the coffee{" "}
             <span role="img" aria-label="coffee">
@@ -37,7 +52,9 @@ export default () => (
             is good). If none of those places are near you no problem! Shoot me
             a text, email, or message me on Instagram and we can set up a time
             for a video call!
-        </p>
+        </Body1>
+
+
         <div class="anoun-footer-container">
             <img
                 src={footerProfile}
@@ -45,16 +62,16 @@ export default () => (
                 width="75px"
                 alt="ANOUN-founder-profile"
             />
-            <h5 id="footer-name">Jaydan Urwin</h5>
-            <p>
+            <Headline5 id="footer-name">Jaydan Urwin</Headline5>
+            <Body1>
                 founder of <span class="anoun-title">ANOUN</span>
-            </p>
+            </Body1>
 
-            <p class="mdc-typography--body">(208) 807-9333</p>
+            <Body1 class="mdc-typography--body">(208) 807-9333</Body1>
 
-            <p class="mdc-typography--body">jaydan.urwin@gmail.com</p>
+            <Body1 class="mdc-typography--body">jaydan.urwin@gmail.com</Body1>
 
-            <p class="mdc-typography--body">
+            <Body1 class="mdc-typography--body">
                 Instagram
                 <img
                     src={instagramOutlineLightTheme}
@@ -62,7 +79,7 @@ export default () => (
                     width="14px"
                     alt="instagram-logo-vector-svg"
                 />
-            </p>
+            </Body1>
         </div>
     </section>
 );
