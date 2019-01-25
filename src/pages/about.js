5e34851f5297b0ddfa0da1b07329f328256cee9a
Urwin <jurwin1@SDGL15069e1e1.local>
2019-01-25 08:57:45 -0700

    wrapped about in Layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,33 @@
 import React from "react";
 import { graphql } from "gatsby";
-
+import Layout from "../components/Layout.js";
 // Styles
 import "../styles/app.scss";
 
 const About = ({ data: { prismicAboutpage } }) => (
-    <React.Fragment>
-        <h1>{prismicAboutpage.data.title.text}</h1>
-        <div
-            dangerouslySetInnerHTML={{
-                __html: prismicAboutpage.data.content.html
-            }}
-        />
-    </React.Fragment>
+  <Layout>
+    <h1>{prismicAboutpage.data.title.text}</h1>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: prismicAboutpage.data.content.html
+      }}
+    />
+  </Layout>
 );
 
 export default About;
 
 export const pageQuery = graphql`
-    query {
-        prismicAboutpage {
-            data {
-                title {
-                    text
-                }
-                content {
-                    html
-                }
-            }
+  query {
+    prismicAboutpage {
+      data {
+        title {
+          text
         }
+        content {
+          html
+        }
+      }
     }
+  }
 `;
