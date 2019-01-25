e92ee4f25387d5ac03372f56389ed2c509a570a3
Urwin <jurwin1@SDGL15069e1e1.local>
2019-01-25 08:33:07 -0700

    added google fonts and normalize via meta data so dropping reset

--- src/components/meta/WebsiteMeta.js
+++ src/components/meta/WebsiteMeta.js
@@ -5,16 +5,26 @@ import config from "../../../config/website.js";
 const MetaData = () => {
   const title = config.title;
   const description = config.description;
-  return <>
+  return (
+    <>
       <Helmet>
-        <html lang='en'/>
+        <html lang="en" />
         <meta charset="utf-8" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato|Playfair+Display:400,900"
+          rel="stylesheet"
+        />
       </Helmet>
-    </>;
+    </>
+  );
 };
 
 export default MetaData;
