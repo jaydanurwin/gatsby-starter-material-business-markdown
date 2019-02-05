10d26be506666eb68ecb5d826b60adefe991623b
Urwin <jurwin1@SDGL15069e1e1.local>
2019-02-05 08:15:32 -0700

    moved viewport and language links out of SEO

--- src/components/meta/SEO.js
+++ src/components/meta/SEO.js
@@ -8,16 +8,6 @@ const SEO = () => {
     return (
         <>
             <Helmet>
-                <html lang="en" />
-                <meta charset="utf-8" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1, width=device-width"
-                />
-                <meta
-                    http-equiv="X-UA-Compatible"
-                    content="IE=edge,chrome=1"
-                />
                 <title>{title}</title>
                 <meta name="description" content={description} />
             </Helmet>
