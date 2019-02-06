26bb246f1f782c2691bc7f5c309b8c6503a27d91
Urwin <jurwin1@SDGL15069e1e1.local>
2019-02-06 13:51:53 -0700

    added MDC card

---
+++ src/components/MDCCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Styles
+import './mdcCard.scss'
+
+const MDCCard = ({ children }) => {
+    return (
+        <>
+            <div className="mdc-card">
+            {children}
+            </div>
+        </>
+    )
+}
+
+export default MDCCard
+
