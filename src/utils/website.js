535939f31e07f9fe28676616fb648955cc24f441
Urwin <jurwin1@SDGL15069e1e1.local>
2019-02-19 15:47:15 -0700

    updated title and description

--- src/utils/website.js
+++ src/utils/website.js
@@ -1,9 +1,9 @@
 module.exports = {
     pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-    title: 'ANOUN | Website & App Design Agency in Boise, ID', // Navigation and Site Title
+    title: 'ANOUN | A Web Design Company in Boise, ID', // Navigation and Site Title
     titleAlt: 'ANOUN', // Title for JSONLD
     description:
-        'ANOUN is a website and app design agency based in Boise, Idaho. We take a people-first approach designing experiences through websites, iPhone apps, and Android apps. We aim to deliver the best design services in Boise, Meridian, and Nampa, Idaho. Contact us to learn more!',
+        'ANOUN is a web design company based in Boise, Idaho. We take a people-first approach designing clean, modern customer experiences through websites and mobile apps. We aim to deliver the best design services in Boise, Meridian, and Nampa, Idaho. Contact us to learn more!',
     headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
     url: 'https://anoun.design', // Domain of your site. No trailing slash!
     siteLanguage: 'en', // Language Tag on <html> element
