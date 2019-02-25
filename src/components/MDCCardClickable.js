884f3059e97d20e6f2a4088f6fc5e252e5f9c389
Urwin <jurwin1@SDGL15069e1e1.local>
2019-02-25 09:19:38 -0700

    added MDCCardClickable component

---
+++ src/components/MDCCardClickable.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// Styles
+import './mdcCardClickable.scss'
+
+const MDCCard = ({ children }) => {
+    return (
+        <>
+            <div class="mdc-card mdc-card--clickable">
+            {children}
+            </div>
+        </>
+    )
+}
+
+export default MDCCard
+
