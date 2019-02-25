0cafa4fb475b38682059eee454d75eb5b23fe302
Urwin <jurwin1@SDGL15069e1e1.local>
2019-02-25 08:53:15 -0700

    not using ogImage

--- src/components/meta/SEO.js
+++ src/components/meta/SEO.js
@@ -5,20 +5,13 @@ import config from '../../utils/website.js'
 const SEO = () => {
     const title = config.title
     const description = config.description
-    const ogImage = config.ogImage
     return (
         <>
             <Helmet>
                 <title>{title}</title>
                 <meta name="description" content={description} />
-                <meta
-                    property="og:title"
-                    content={title}
-                />
-                <meta
-                    property="og:description"
-                    content={description}
-                />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
                 <meta
                     property="og:image"
                     content="https://anoun.design/images/anoun-share-image.png"
