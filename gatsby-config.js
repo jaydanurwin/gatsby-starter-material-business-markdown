fe7304042805ae7289edd6dbc5aa083e3f95851b
Urwin <jurwin1@sdgl15069e1e1.lan>
2019-02-28 06:47:09 -0700

    added link resolver to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
             options: {
                 repositoryName: `anoun`,
                 accessToken: `${process.env.API_KEY}`,
+                linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
             }
         },
         {
