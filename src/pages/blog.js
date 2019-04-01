0956d8ba408a1e2c547c88fd8a04de30f90d3396
Jaydan Urwin <jaydan@anoun.design>
2019-04-01 20:05:31 +0000

    larger blog post titles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -21,12 +21,12 @@ class BlogPage extends React.Component {
           const title = node.frontmatter.title || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
+              <h2
               >
                 <Link style={{ textDecoration: `underline` }} to={node.fields.slug}>
                   {title}
                 </Link>
-              </h3>
+              </h2>
               <small>{node.frontmatter.date}</small>
               <p
                 dangerouslySetInnerHTML={{
