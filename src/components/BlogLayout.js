f1296cd6948e9934ecfba923293ce81c94315dc0
Jaydan Urwin <jaydan@anoun.design>
2019-04-15 20:18:12 +0000

    updated blog layout with new Footer

--- src/components/BlogLayout.js
+++ src/components/BlogLayout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "./Header"
+import Footer from "./Footer"
 
 // import instagramIcon from "../images/icons/instagram-round-white-24px.svg"
 
@@ -13,29 +13,7 @@ class BlogLayout extends React.Component {
       <div className={styles.siteContainer}>
         <Header />
         <div className={styles.siteMain}>{children}</div>
-        <footer className={styles.siteFooter}>
-          {/* <a
-            href="https://www.instagram.com/anoun.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram{" "}
-            <img
-              src={instagramIcon}
-              id="igLogo"
-              width="14px"
-              alt="instagram-logo-vector-svg"
-            />
-          </a>*/}
-          <Link to="/">Home</Link>
-          {" • "} <Link to="/blog">Blog</Link> {"• "}
-          <Link to="/about">About</Link>
-          {/* <div className={styles.siteInfo}>
-          © {new Date().getFullYear()}, Built with <a href="https://www.sanity.io">Sanity</a> &amp;
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </div> */}
-        </footer>
+        <Footer />
       </div>
     )
   }
