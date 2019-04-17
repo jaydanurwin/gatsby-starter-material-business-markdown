2b910e8ac2a6cfa3da36d6e49a6dd65f75d9a8bd
Jaydan Urwin <jaydan@anoun.design>
2019-04-17 04:22:53 +0000

    format run

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import { Link } from "gatsby"
-import React, {Component} from "react"
+import React, { Component } from "react"
 
 import styles from "./layout.module.scss"
 
 class Header extends Component {
   render() {
-    const ListLink = props =>
-    <li>
-      <Link to={props.to} activeClassName={styles.activeLink}>
-      {props.children}</Link>
-    </li>
+    const ListLink = props => (
+      <li>
+        <Link to={props.to} activeClassName={styles.activeLink}>
+          {props.children}
+        </Link>
+      </li>
+    )
     return (
       <header className={styles.siteHeader}>
         {/* <Link to={`/`}>
@@ -18,12 +20,12 @@ class Header extends Component {
       </h3>
     </Link> */}
         <ul>
-            <ListLink to={`/`}>home</ListLink>
-            <ListLink to={`/blog`}>blog</ListLink>
-            <ListLink to={`/about`}>about</ListLink>
+          <ListLink to={`/`}>home</ListLink>
+          <ListLink to={`/blog`}>blog</ListLink>
+          <ListLink to={`/about`}>about</ListLink>
         </ul>
       </header>
-    );
+    )
   }
 }
 
