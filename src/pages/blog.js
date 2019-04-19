843ba18420a6b7b6e9635b38d5036e544b15d25c
Jaydan Urwin <jaydan@anoun.design>
2019-04-19 14:33:10 -0600

    added spacing to blog page and home page section 3

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,6 +19,7 @@ class BlogPage extends React.Component {
         <h1>
           Posts by <span className="anoun-title">ANOUN</span>
         </h1>
+        <section className="blog-main__section">
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
@@ -40,6 +41,7 @@ class BlogPage extends React.Component {
             </div>
           )
         })}
+        </section>
       </BlogLayout>
     )
   }
