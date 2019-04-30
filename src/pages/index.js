48b12cf41c47feca454d067b85e512931edebf1f
Jaydan Urwin <42281662+jaydanurwin@users.noreply.github.com>
2019-04-30 08:19:23 -0600

    title is still broken

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,12 +16,11 @@ import "../styles/app.scss"
 
 class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO />
+      <Layout>
+        <SEO title={siteTitle}/>
         <MetaLinks />
         <section className="anoun-home--section1">
           <h1>
