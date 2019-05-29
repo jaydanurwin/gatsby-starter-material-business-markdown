e157ef9279cd23ee3d196a2a39bf49718f53f639
Jaydan Urwin <42281662+jaydanurwin@users.noreply.github.com>
2019-05-29 09:39:13 -0600

    Update website.js

--- content/website.js
+++ content/website.js
@@ -1,19 +1,19 @@
 module.exports = {
   // SEO
   title: "Gatsby Starter #1 | ANOUN | Markdown", // Navigation and Site Title
-  titleTemplate: "%s · Gatsby Starter #1",
+  titleTemplate: "%s · Gatsby Starter #1", // Template for Page Titles
   description: "This is Gatsby Starter #1 by ANOUN with content sourcing from Markdown files. Don't be shy, clone it on Github if you'd like!",
-  siteUrl: "https://gatsby-starter-anoun-1-markdown.netlify.com/", // Domain of your site. No trailing slash!
-  image: "/images/anoun-share-image.png", // Used for SEO
+  siteUrl: "https://gatsby-starter-anoun-1-markdown.netlify.com", // Domain of your site. No trailing slash!
+  image: "/images/anoun-share-image.png", // Used for SEO Share Image
 
   // Manifest
   icon: "src/favicon.png", // Used for manifest favicon generation
   short_name: "ANOUN", // shortname for manifest. MUST be shorter than 12 characters
-  owner: "ANOUN", // Author for schemaORGJSONLD
+  owner: "ANOUN", // Author
   theme_color: "#663399",
   background_color: "#ffffff",
 
   // Social and Analytics
   twitterUsername: "@anouncompany", // Twitter Username
-  googleAnalyticsID: "",
+  googleAnalyticsID: "", // Google Analytics Tag ID
 }
