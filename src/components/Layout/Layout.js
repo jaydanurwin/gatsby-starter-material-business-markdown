bbb2a7d5c110ce40c72fe75a1bc1cb87ce5b62cf
Jaydan <jaydan@anoun.company>
2019-05-30 22:17:33 -0600

    Refactored MetaLinks component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
-import MetaLinks from "../MetaLinks"
+import MetaLinks from "../MetaLinks/MetaLinks"
 
 import styles from "./layout.module.scss"
 
