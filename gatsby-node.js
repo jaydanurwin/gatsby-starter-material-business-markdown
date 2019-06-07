c0c78192a9cbfba566f2c521be03ee67a0b45608
Jaydan <jaydan@anoun.company>
2019-06-06 22:05:16 -0600

    filtering posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,49 +6,51 @@ exports.createPages = ({ graphql, actions }) => {
 
     const blogPost = path.resolve(`./src/templates/blog-post.js`)
     return graphql(
-        `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
+      `
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "\/posts/" } }
+            sort: { fields: [frontmatter___date], order: DESC }
+            limit: 1000
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
               }
             }
           }
         }
-      }
-    `
+      `
     ).then(result => {
-        if (result.errors) {
-            throw result.errors
-        }
+      if (result.errors) {
+        throw result.errors
+      }
 
-        // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges
+      // Create blog posts pages.
+      const posts = result.data.allMarkdownRemark.edges
 
-        posts.forEach((post, index) => {
-            const previous = index === posts.length - 1 ? null : posts[index + 1].node
-            const next = index === 0 ? null : posts[index - 1].node
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
 
-            createPage({
-                path: post.node.fields.slug,
-                component: blogPost,
-                context: {
-                    slug: post.node.fields.slug,
-                    previous,
-                    next,
-                },
-            })
+        createPage({
+          path: post.node.fields.slug,
+          component: blogPost,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
         })
+      })
 
-        return null
+      return null
     })
 }
 
